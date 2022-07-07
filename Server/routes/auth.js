48fac20c86cf829ddcc2fcaea9ac2e6a6ee81953
nitishsaini706 <nitishsaini706@gmail.com>
2022-07-07 12:21:25 +0530

    creating schema

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const mongoose = require('mongoose');
+const user = mongoose.model('User')
 
 router.get('/', (req,res)=>{
     res.send("Hello");
@@ -10,8 +11,27 @@ router.post('/signup',(req,res)=>{
     const {name,email,password} =req.body;
     if(!email || !name || !password)
     {
-        req.json({error:"please add all fields"})
+        req.status(442).json({error:"please add all fields"})
     }
+    user.findOne({email:email}).then((savedUser)=>{
+        if(savedUder){
+            return res.status(422).json({error:"user already exist with this email"})
+        }
+        const user =  new User({
+            email,password,name
+        })
+
+        user.svae()
+        .then(user => {
+            res.json({message:"Saved credentials"})
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    })
 })
 
 module.exports = router;
