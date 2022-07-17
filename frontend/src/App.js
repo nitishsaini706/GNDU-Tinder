2fc864484a7340f62adf3841f1fa65c396472269
nitishsaini706 <nitishsaini706@gmail.com>
2022-07-17 06:26:23 +0530

    adding routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,37 @@
 import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/login"
+import Profile from "./pages/Profile/Profile"
+import {
+  Router,
+  Switch,
+  Route,
 
+} from "react-router-dom";
 
 function App() {
-  return <Home/>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+
+        <Route exact path="/login">
+          <Login/>
+        </Route>
+
+        <Route exact path="/register">
+          <Register/>
+        </Route>
+
+        <Route exact path="/profile/:username">
+          <Profile/>
+        </Route>
+      </Switch>
+      
+    </Router>
+  );
 }
 
 export default App;
